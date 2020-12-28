17adf2cea85703d500ba6d2be4594af67c159e13
Jael Alexander Rivera <alexander.rivera@avaldigitallabs.com>
2020-12-28 10:54:06 -0500

    method doOnNext

--- src/main/java/co/com/rective/ReactorApplication.java
+++ src/main/java/co/com/rective/ReactorApplication.java
@@ -1,6 +1,6 @@
 package co.com.rective;
 
-import io.reactivex.Single;
+import io.reactivex.Observable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,11 +17,13 @@ public class ReactorApplication implements CommandLineRunner {
 
   public void reactor() {
     Mono.just(new Person(1, "project", "reactor"))
+        .doOnNext(person -> log.info("ReactorApplication::reactor doOnNext {}", person.toString()))
         .subscribe(person -> log.info("ReactorApplication::reactor {}", person.toString()));
   }
 
   public void rxJava() {
-    Single.just(new Person(1, "project", "reactor"))
+    Observable.just(new Person(1, "project", "reactor"))
+        .doOnNext(person -> log.info("ReactorApplication::rxJava doOnNext {}", person.toString()))
         .subscribe(person -> log.info("ReactorApplication::rxJava {}", person.toString()));
   }
 
